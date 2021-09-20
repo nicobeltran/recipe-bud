b2d8852804aedc907440a37f59dc78b078c836af
Nico Beltran <nicobeltran9@gmail.com>
2021-09-20 14:30:00 -0700

    Added ingredients controller

---
+++ server/app/controllers/api/v1/ingredients_controller.rb
@@ -0,0 +1,40 @@
+class Api::V1::IngredientsController < ApplicationController
+    before_action :find_ingredient, only: [:show, :update, :destroy]
+
+    def index
+        @ingredients = Ingredient.all
+        render json: @ingredients
+    end
+
+    def show
+        render json: @ingredient
+    end
+
+    def create
+        @ingredient = Ingredient.new(ingredient_params)
+        if @ingredient.save
+            render json: { Done: {}}
+        else
+            render json: { error: 'Unable to create ingredient.'}, status: 400
+        end
+    end
+
+    def destroy
+        if @ingredient
+            @ingredient.destroy
+            render json: { message: 'Ingredient removed successfully.'}, status: 200
+        else
+            render json: { error: 'Unable to remove ingredient.'}, status: 400
+        end
+    end
+
+    private
+
+    def ingredient_params
+        params.require(:ingredient).permit(:quantity, :item, :recipe_id)
+    end
+
+    def find_ingredient
+        @ingredient = Ingredient.find(params[:id])
+    end
+end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users do
-        resources :recipes
+        resources :recipes do
+          resources :ingredients
+        end
       end
     end
   end
